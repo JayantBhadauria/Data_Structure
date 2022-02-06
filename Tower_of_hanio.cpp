a9ab595676be50f4cf263b1609c5179f971ea239
Jayant Bhadauria <63440315+JayantBhadauria@users.noreply.github.com>
2022-02-06 15:26:50 +0530

    Tower of hanoi problem

---
+++ Tower_of_hanio.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void toh(int num,int from,int aux,int to){
+    if(num==1){
+        cout<<"Move "<<num<<" plate from rod "<<from<<" to rod "<<to<<endl;
+        return;
+    }
+    toh(num-1,from,aux,to);
+    cout<<"Move "<<num<<" plate from rod "<<from<<" to rod "<<aux<<endl;
+    toh(num-1,aux,to,from);
+    return;
+}
+int main(){
+    int num;
+    cout<<"How many number of plates : ";
+    cin>>num;
+    toh(num,0,1,2);
+}
