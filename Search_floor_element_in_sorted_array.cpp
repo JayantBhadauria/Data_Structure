03f1e39ab953aceef3070a159397af53fde396d2
Jayant Bhadauria <jayantbhadauria@gmail.com>
2022-03-12 19:30:07 +0530

    Search floor element in a sorted array
    Binary Search

---
+++ Search_floor_element_in_sorted_array.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int floor_Element_Search(int *arr,int size,int k){
+//     int i=0,res=-1;
+//     for(;i<size;i++){
+//         if(arr[i]<k){
+//             res=i;
+//         }
+//     }
+//     return res;
+// }
+
+//*****OPTIMIZED*****
+
+int floor_Element_Search(int *arr,int size,int k){
+    int left=0,right=size-1,mid=left+(right-left)/2,res=-1;
+    while(left<=right){
+        mid=left+(right-left)/2;
+        if(arr[mid]==k){
+            return mid;
+        }
+        else if(arr[mid]>k){
+            right=mid-1;
+        }
+        else if(arr[mid]<k){
+            res=mid;
+            left=mid+1;
+        }
+    }
+    return res;
+}
+
+int main(){
+    int size,ele;
+    cout<<"Size : ";
+    cin>>size;
+    int arr[size];
+    cout<<"Enter "<<size<<" sorted elements : \n";
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter element to search : ";
+    cin>>ele;
+    if(floor_Element_Search(arr,size,ele)>0){
+    cout<<ele<<" is located at "<<floor_Element_Search(arr,size,ele)<<"th position";
+    }
+    else{
+        cout<<ele<<" is not present";
+    }
+    return 0;
+}
