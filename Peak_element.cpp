ad409bff3bad4a9d2ba72addfb227811159bcfa9
Jayant Bhadauria <jayantbhadauria@gmail.com>
2022-03-13 18:42:07 +0530

    Binary Search

---
+++ Peak_element.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int Peak_element(int *arr,int size){
+//     int i=1;
+//     for(;i<size-1;i++){
+//         if(arr[i-1]<=arr[i] and arr[i+1]<=arr[i]){
+//             return arr[i];
+//         }
+//     }
+//     if(arr[0]>=arr[1]){
+//         return arr[0];
+//     }
+//     return arr[i];
+// }
+
+//*****OPTIMIZED*****
+
+int Peak_element(int *arr,int size){
+    int left=0,right=size-1,mid=left+(right-left)/2;
+    while(left<=right){
+        mid=left+(right-left)/2;
+        if(arr[mid]>=arr[mid-1] and arr[mid]>=arr[mid+1]){
+            return arr[mid];
+        }
+        else if(mid-1>=0 and arr[mid]<=arr[mid-1]){
+            right=mid-1;
+        }
+        else if(mid+1<=size and  arr[mid+1]>=arr[mid]){
+            left=mid+1;
+        }
+    }
+    return arr[mid];
+}
+
+int main(){
+    int size,i=0;
+    cout<<"Size : ";
+    cin>>size;
+    cout<<"Enter "<<size<<" elements :\n";
+    int arr[size];
+    for(i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    int ans=Peak_element(arr,size);
+    cout<<ans;
+    return 0;
+}
